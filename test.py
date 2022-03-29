e48635a41be6f5986b56437d72ef8b7d75509b01
LyokoJeremie <lucheng989898@gmail.com>
2022-03-29 20:53:35 +0800

    add keyPressEvent test

--- test.py
+++ test.py
@@ -1,9 +1,46 @@
 from cv2 import cv2
 
-from PhantasyIslandPythonRemoteControl import AirplaneManager
+from PhantasyIslandPythonRemoteControl import get_airplane_manager
+from PhantasyIslandPythonRemoteControl.control_command import AirplaneController
+
+
+def keyPressEvent(k: int, a: AirplaneController, ):
+    if k == -1:  # no press
+        return
+    elif k == 119:  # w
+        a.forward(100)
+        return
+    elif k == 97:  # a
+        a.left(100)
+        return
+    elif k == 115:  # s
+        a.back(100)
+        return
+    elif k == 100:  # d
+        a.right(100)
+        return
+    elif k == 113:  # q
+        a.rotate(-90)
+    elif k == 101:  # e
+        a.rotate(90)
+        return
+    elif k == 114:  # r
+        a.up(100)
+    elif k == 102:  # f
+        a.down(100)
+        return
+    elif k == 32:  # Space
+        a.takeoff(100)
+        return
+    elif k == 27:  # ESC
+        a.land()
+        return
+
+    pass
+
 
 if __name__ == '__main__':
-    m = AirplaneManager()
+    m = get_airplane_manager()
 
     # print('airplanes_table', m.airplanes_table)
 
@@ -11,7 +48,9 @@ if __name__ == '__main__':
 
     # print('airplanes_table', m.airplanes_table)
 
-    a = m.get_airplane('COM3')
+    a: AirplaneController = m.get_airplane('COM3')
+
+    print(m.start())
 
     if a:
         cv2.namedWindow(f'{a.keyName} front', cv2.WINDOW_NORMAL)
@@ -20,7 +59,7 @@ if __name__ == '__main__':
         while True:
             cv2.imshow(f'{a.keyName} front', a.get_camera_front_img())
             cv2.imshow(f'{a.keyName} down', a.get_camera_down_img())
-            cv2.waitKey(50)
+            keyPressEvent(cv2.waitKey(50), a)
             m.flush()
         pass
 
